1a1628e3380d0c8630a159f6d8fcde9e942900fd
Sillson <Sillson88@gmail.com>
2015-08-23 23:11:34 -0700

    simple JSON feed up and running

--- Gemfile
+++ Gemfile
@@ -2,13 +2,22 @@ source 'https://rubygems.org'
 
 
 gem 'rails', '4.2.3'
-
 gem 'rails-api'
-
-gem 'spring', :group => :development
-
-
 gem 'pg'
+
+group :development do 
+	gem 'spring'
+	gem 'spring-commands-rspec'
+end
+
+group :test do 
+	gem 'shoulda-matchers', require: false
+end
+
+group :development, :test do 
+	gem 'rspec-rails', '~> 3.0'
+end
+
 
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+[
+	{ full_name: "Huxley Furpaw", email: "Doglyfe@gmail.com",      phone: "531 123 2445"},
+	{ full_name: "Mark Smith",    email: "mark.smith@example.com", phone: "034 6789 1234"},
+  	{ full_name: "Tom Clark",     email: "tom.clark@example.com",  phone: "033 4321 9876"},
+  	{ full_name: "Sue Palmer",    email: "sue.palmer@example.com", phone: "034 9876 1234"},
+  	{ full_name: "Kate Lee",      email: "kate.lee@example.com",   phone: "033 6789 4321"}
+].each do |customer_attributes|
+	Customer.create(customer_attributes)
+end
